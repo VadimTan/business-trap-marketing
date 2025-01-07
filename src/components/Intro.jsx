d692f0e6505309385bd976d6845e11c398a23d25
VadimTan <v.tanasiev1@gmail.com>
2025-01-07 13:01:35 +0200

    added animation and info blocks

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,17 +1,68 @@
 import { ReactTyped } from 'react-typed';
 import '../styles/intro.css';
+import { InfoCard } from './reusable/InfoCard';
+import { Slide } from 'react-awesome-reveal';
+import { useState, useEffect } from 'react';
 
 export const Intro = () => {
+  const [showSlides, setShowSlides] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSlides(true);
+    }, 1650);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className='flex justify-center'>
+    <div className='flex flex-col justify-center overflow-hidden'>
       <ReactTyped
         strings={[
-          `<h1 class='text-[96px] select-none text-[#3b3b3b] font-[Audiowide]'>Business Trap Marketing</h1>`,
+          `<h1 class='text-[96px] text-center select-none text-[#3b3b3b] font-[Audiowide]'>Business Trap Marketing</h1>`,
         ]}
         typeSpeed={40}
         backSpeed={50}
         showCursor={false}
       />
+
+      {showSlides && (
+        <div className='flex gap-10 px-5'>
+          <Slide
+            triggerOnce
+            direction='left'
+            delay={600}
+            damping={0.2}>
+            <InfoCard
+              text={`Standard chunk of Lorem Ipsum used since the 1500s is showed below for
+          those interested.`}
+              title={'Products'}
+            />
+          </Slide>
+          <Slide
+            delay={800}
+            direction='up'
+            triggerOnce
+            damping={0.2}>
+            <InfoCard
+              text={`Standard chunk of Lorem Ipsum used since the 1500s is showed below for
+          those interested.`}
+              title={'About Us'}
+            />
+          </Slide>
+          <Slide
+            delay={1000}
+            direction='right'
+            triggerOnce
+            damping={0.2}>
+            <InfoCard
+              text={`Standard chunk of Lorem Ipsum used since the 1500s is showed below for
+          those interested.`}
+              title={'Marketing'}
+            />
+          </Slide>
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/components/reusable/InfoCard.jsx
@@ -0,0 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
+
+// eslint-disable-next-line react/prop-types
+export const InfoCard = ({ title, text }) => {
+  return (
+    <div className='bg-[rgba(164,161,161,0.21)] min-h-[400px] rounded-[16px] [box-shadow:0_4px_30px_rgba(0,_0,_0,_0.1)] backdrop-filter backdrop-blur-[1.9px] border-[1px] border-[solid] border-[rgba(169,159,159,0.25)]'>
+      <div className='flex flex-col gap-10 py-5 px-10 items-start'>
+        <FontAwesomeIcon
+          fade={true}
+          color='gray'
+          size='xl'
+          icon={faCircleInfo}
+        />
+        <div className='flex flex-col gap-2'>
+          <h4 className='text-white'>{title}</h4>
+          <p className='text-[#a3a3a3]'>{text}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
